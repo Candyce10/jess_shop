0c08ac98fa1f462ab6ce26d9c7e012c0c544c22b
Candyce10 <candycematthews@yahoo.com>
2022-07-21 23:52:54 -0700

    created home and about templates

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.views import View 
-from django.http import HttpResponse 
+from django.http import HttpResponse
+from django.views.generic.base import TemplateView
 
 
 class Home(View):
     def get(self, request):
         return HttpResponse("Gainzz & Curvezz Home")
+
+class About(View):
+
+    def get(self, request):
+        return HttpResponse("Gainzz & Curvezz About")
+
+class Home(TemplateView):
+    template_name = "home.html"
