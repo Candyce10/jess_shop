0f8285887806edb191614bde8b0844b29750c2e5
Candyce10 <candycematthews@yahoo.com>
2022-07-22 00:34:00 -0700

    added fake database data

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.Home.as_view(), name="home"),
     path('about/', views.About.as_view(), name="about"),
+    path('products/', views.ProductList.as_view(), name="product_list")
 ]
