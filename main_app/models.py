e1fc0e635f01945bfd3e14f56ccc98bb1efffc7f
Candyce10 <candycematthews@yahoo.com>
2022-07-22 01:33:38 -0700

    added to database

--- main_app/models.py
+++ main_app/models.py
@@ -4,8 +4,8 @@ class Product(models.Model):
 
     name = models.CharField(max_length=100)
     img = models.CharField(max_length=250)
-    description = models.TextField(max_length=500)
-    ingredients = models.TextField(max_length=500)
+    description = models.TextField(max_length=1000)
+    ingredients = models.TextField(max_length=1000)
     
     def __str__(self):
         return self.name
