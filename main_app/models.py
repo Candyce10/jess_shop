0165d8faa1aeb150b310398f9c3bcf082a0011d1
Candyce10 <candycematthews@yahoo.com>
2022-08-01 13:46:50 -0700

    changed url to slug

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Product(models.Model):
 
@@ -6,9 +7,13 @@ class Product(models.Model):
     img = models.CharField(max_length=250)
     description = models.TextField(max_length=1000)
     ingredients = models.TextField(max_length=1000)
+    slug = models.SlugField(null=True)
     
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse("product_detail", kwargs={"slug": self.slug})
 
     class Meta:
         ordering = ['name']
