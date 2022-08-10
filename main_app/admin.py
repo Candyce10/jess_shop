b9fd165e6552295c2d1f3432771d7c4fef37fdd1
Candyce10 <candycematthews@yahoo.com>
2022-08-10 09:32:41 -0700

    added billing model to admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Customer, Product, Order, OrderProduct, ShippingAddress
+from .models import Customer, Product, Order, OrderProduct, ShippingAddress, BillingAddress
 
 admin.site.register(Product) 
 admin.site.register(Order) 
 admin.site.register(OrderProduct)
 admin.site.register(Customer)
 admin.site.register(ShippingAddress)
+admin.site.register(BillingAddress)
